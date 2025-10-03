13eb1ebaa2ca743eba009226c1bf9acde13c99ce
zippy0626 <zippy0626@gmail.com>
2025-10-03 12:55:27 -0400

    feat: added basic themes

---
+++ src/components/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffffff',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  }
+})
+
+export { theme };
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// theme stuff
+import { ThemeProvider } from "@mui/material";
+import { theme } from './components/theme.ts';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
