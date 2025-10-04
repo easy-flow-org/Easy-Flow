2bbc074ffc8bd9fbdfdb992d94b55dc5d094d516
zippy0626 <zippy0626@gmail.com>
2025-10-04 13:02:40 -0400

    refactor: add generic section wrapper

---
+++ src/components/LandingPage/Section.tsx
@@ -0,0 +1,14 @@
+import { styled } from "@mui/material"
+import type { PropsWithChildren } from "react"
+
+const StyledSection = styled("section")(({ theme }) => ({
+  width: "100vw",
+  height: "100vh",
+}))
+
+// Props with children allow us to pass children ele in Section
+export default function Section({children} : PropsWithChildren) {
+  return (
+    <StyledSection>{children}</StyledSection>
+  )
+}
