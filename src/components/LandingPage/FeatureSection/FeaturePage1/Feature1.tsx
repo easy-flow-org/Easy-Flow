290c06f81506332a3361a9dbda8d8fcbe3e495f7
zippy0626 <zippy0626@gmail.com>
2025-10-21 19:32:47 -0400

    refactor: made card logic cleaner

--- src/components/LandingPage/FeatureSection/FeaturePage1/Feature1.tsx
+++ src/components/LandingPage/FeatureSection/FeaturePage1/Feature1.tsx
@@ -1,9 +1,47 @@
-import { Stack, Typography, Card, CardContent, Box } from "@mui/material";
+import { ArrowRight, ArrowRightOutlined } from "@mui/icons-material";
+import { Stack, Typography, Card, CardContent, Box, CardActions, Button } from "@mui/material";
 
 export default function Feature1() {
   const cardMinWidth = { xs: 230, sm: 330 };
   const previewBoxSizes = { xs: 230, sm: 330 };
   const previewBoxMargins = { xs: "auto" };
+
+  const cards = [
+    {
+      title: "Import Your Syllabus in Seconds",
+      desc: "Send class deadlines to your calendar.",
+    },
+    {
+      title: "Stay on Top of Every Task",
+      desc: "All your notes and to-dos in one place.",
+    },
+    {
+      title: "Focus Without Distractions",
+      desc: "Block noise, lock in, and get work done.",
+    },
+  ].map((card) => (
+    <Card sx={{ minWidth: cardMinWidth }}>
+      <CardContent>
+        <Typography variant="h6" align={"center"}>
+          {card.title}
+        </Typography>
+        <Typography variant="body1" color="text.secondary" align={"center"}>
+          {card.desc}
+        </Typography>
+        <Box
+          sx={{
+            bgcolor: "whitesmoke",
+            height: previewBoxSizes,
+            width: previewBoxSizes,
+            borderRadius: 2,
+            mx: previewBoxMargins,
+            mb: previewBoxMargins,
+            mt: 1,
+          }}
+        ></Box>
+      </CardContent>
+    </Card>
+  ));
 
   return (
     <>
@@ -18,69 +56,7 @@ export default function Feature1() {
           Keep Up Effortlessly
         </Typography>
         <Stack direction={"row"} justifyContent={"center"} marginTop={4} flexWrap={"wrap"} gap={3}>
-          <Card sx={{ minWidth: cardMinWidth }}>
-            <CardContent>
-              <Typography variant="h6" align={"center"}>
-                Import Your Syllabus in Seconds
-              </Typography>
-              <Typography variant="body1" color="text.secondary" align={"center"}>
-                Send class deadlines to your calendar.
-              </Typography>
-              <Box
-                sx={{
-                  bgcolor: "whitesmoke",
-                  height: previewBoxSizes,
-                  width: previewBoxSizes,
-                  borderRadius: 2,
-                  mx: previewBoxMargins,
-                  mb: previewBoxMargins,
-                  mt: 1,
-                }}
-              ></Box>
-            </CardContent>
-          </Card>
-          <Card sx={{ minWidth: cardMinWidth }}>
-            <CardContent>
-              <Typography variant="h6" align={"center"}>
-                Stay on Top of Every Task
-              </Typography>
-              <Typography variant="body1" color="text.secondary" align={"center"}>
-                All your notes and to-dos in one place.
-              </Typography>
-              <Box
-                sx={{
-                  bgcolor: "whitesmoke",
-                  height: previewBoxSizes,
-                  width: previewBoxSizes,
-                  borderRadius: 2,
-                  mx: previewBoxMargins,
-                  mb: previewBoxMargins,
-                  mt: 1,
-                }}
-              ></Box>
-            </CardContent>
-          </Card>
-          <Card sx={{ minWidth: cardMinWidth }}>
-            <CardContent>
-              <Typography variant="h6" align={"center"}>
-                Focus Without Distractions
-              </Typography>
-              <Typography variant="body1" color="text.secondary" align={"center"}>
-                Block noise, lock in, and get work done.
-              </Typography>
-              <Box
-                sx={{
-                  bgcolor: "whitesmoke",
-                  height: previewBoxSizes,
-                  width: previewBoxSizes,
-                  borderRadius: 2,
-                  mx: previewBoxMargins,
-                  mb: previewBoxMargins,
-                  mt: 1,
-                }}
-              ></Box>
-            </CardContent>
-          </Card>
+          {cards}
         </Stack>
       </Stack>
     </>
