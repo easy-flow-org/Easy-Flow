0d0617b7bd6290a7b0e4a197ee539d49806e759b
zippy0626 <zippy0626@gmail.com>
2025-10-21 20:12:07 -0400

    chore: removed router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Feature1 from "./components/LandingPage/FeatureSection/FeaturePage1/Feature1"
-import Feature2 from "./components/LandingPage/FeatureSection/FeaturePage2/Feature2"
+import Feature1 from "./components/LandingPage/FeatureSection/FeaturePage1/Feature1";
+import Feature2 from "./components/LandingPage/FeatureSection/FeaturePage2/Feature2";
 import Hero from "./components/LandingPage/HeroSection/Hero";
 import Section from "./components/LandingPage/Section";
 
@@ -10,26 +8,6 @@ import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Landing Page */}
-        <Route
-          path="/"
-          element={
-            <Section>
-              <Hero />
-            </Section>
-          }
-        />
-
-        {/* Auth Pages */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* Add other routes here later like /shop, /admin, etc */}
-      </Routes>
-    </Router>
-  );
     <>
       <Section>
         <Hero />
@@ -41,7 +19,7 @@ function App() {
         <Feature2 />
       </Section>
     </>
-  )
+  );
 }
 
 export default App;
