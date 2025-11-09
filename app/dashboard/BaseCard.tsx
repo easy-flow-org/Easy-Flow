437e28f7ee668339fd88d761308bbe0a7c77ec9d
malkhut <183023495+MalkhutHB@users.noreply.github.com>
2025-11-09 17:11:05 -0500

    style: small border tweak

--- app/dashboard/BaseCard.tsx
+++ app/dashboard/BaseCard.tsx
@@ -5,15 +5,15 @@ export default function BaseCard({children} : {children: React.ReactElement}) {
                 boxShadow: '0px 5px 7px 1px rgba(0, 0, 0, 0.1)',
                 boxSizing: 'border-box',
                 borderRadius: '1.475rem',
-                //borderStyle: 'solid',
-                borderColor: '#b9b9b9ff',
+                borderStyle: 'solid',
+                borderColor: '#c5c5c5b4',
                 borderWidth: '1px',
                 //borderLeft: '4px solid #3987e0ff',
                 
                 width: '100%',
                 height: '100%', 
                 
-                padding: '0rem 1.875rem 0rem 1rem',
+                padding: '1rem 1.875rem 0rem 1rem',
                 color: 'black',
                 backgroundColor: '#F5F5F5',
 
--- app/dashboard/CourseCard.tsx
+++ app/dashboard/CourseCard.tsx
@@ -5,8 +5,8 @@ export default function CourseCard() {
                 boxShadow: '0px 5px 7px 1px rgba(0, 0, 0, 0.1)',
                 boxSizing: 'border-box',
                 borderRadius: '1.475rem',
-                //borderStyle: 'solid',
-                borderColor: '#b9b9b9ff',
+                borderStyle: 'solid',
+                borderColor: '#c5c5c5b4',
                 borderWidth: '1px',
                 //borderLeft: '4px solid #3987e0ff',
                 
