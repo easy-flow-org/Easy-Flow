28a3ccafec804d9cf527a6de5ab865f6b306c023
zippy0626 <zippy0626@gmail.com>
2025-11-20 00:23:08 -0500

    feat: added dummy Course content

---
+++ lib/dummyContent.ts
@@ -0,0 +1,34 @@
+const dummyContent = {
+  courses: [
+    {
+      id: 1,
+      title: "MTH 4000",
+      date: "02-10-2025",
+      time: "9:00 AM",
+      description: "Introduction to calculus and mathematical reasoning"
+    },
+    {
+      id: 2,
+      title: "CSC 2200",
+      date: "02-11-2025",
+      time: "11:00 AM",
+      description: "Fundamentals of programming, algorithms, and data structures"
+    },
+    {
+      id: 3,
+      title: "ENG 2100",
+      date: "02-12-2025",
+      time: "2:00 PM",
+      description: "Great works of literature from classical to modern periods"
+    },
+    {
+      id: 4,
+      title: "HIS 1001",
+      date: "02-13-2025",
+      time: "10:00 AM",
+      description: "Survey of early world history and major civilizations"
+    }
+  ]
+}
+
+export default dummyContent
