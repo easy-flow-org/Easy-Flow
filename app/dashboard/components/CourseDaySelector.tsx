21d6bedae63b7e9e97f046c0c0dc8318e894576f
zippy0626 <zippy0626@gmail.com>
2025-11-25 14:00:22 -0500

    chore: added required attribute to selector

--- app/dashboard/components/CourseDaySelector.tsx
+++ app/dashboard/components/CourseDaySelector.tsx
@@ -44,7 +44,7 @@ export default function CourseDaySelector() {
     <FormControl color="secondary" sx={{ width: "100%", maxWidth: "320px" }}>
       {/* Hidden input so FormData will include the selected days */}
       <input type="hidden" name="courseDays" value={totalDays.join(',')} />
-      <InputLabel id="course-days-input">Course Days</InputLabel>
+      <InputLabel id="course-days-input">Course Days *</InputLabel>
       <Select
         labelId="course-days-input"
         id="select-weekdays"
@@ -54,6 +54,7 @@ export default function CourseDaySelector() {
         input={<OutlinedInput label="Course Days" />}
         renderValue={(selected) => selected.join(', ')}
         MenuProps={MenuProps}
+        required
       >
         {weekDays.map((day) => (
           <MenuItem key={day} value={day}>
