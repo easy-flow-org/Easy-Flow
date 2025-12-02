426406682ee7559037ad7b33ece139092dbf3fbf
malkhut <183023495+MalkhutHB@users.noreply.github.com>
2025-12-02 15:12:12 -0500

    feat: nav buttons for course & task pages

--- app/dashboard/components/IconPicker.tsx
+++ app/dashboard/components/IconPicker.tsx
@@ -1,4 +1,4 @@
-import { SpaceDashboardOutlined, SpaceDashboard, CalendarMonthOutlined, CalendarMonth, OfflineBoltOutlined, OfflineBolt } from "@mui/icons-material"
+import { SpaceDashboardOutlined, SpaceDashboard, CalendarMonthOutlined, CalendarMonth, OfflineBoltOutlined, OfflineBolt, AutoStories, AutoStoriesOutlined, FolderSpecial, FolderSpecialOutlined } from "@mui/icons-material"
 import { SxProps } from "@mui/material";
 
 export default function IconPicker({ icon, ...props }: {icon: String, sx?: SxProps}) {
@@ -17,5 +17,15 @@ export default function IconPicker({ icon, ...props }: {icon: String, sx?: SxPro
             return <OfflineBolt {...props}/>;
         case "OfflineBoltOutlined":
             return <OfflineBoltOutlined {...props}/>;
+        case "AutoStories":
+            return <AutoStories {...props}/>;
+        case "AutoStoriesOutlined":
+            return <AutoStoriesOutlined {...props}/>;
+        case "FolderSpecial":
+            return <FolderSpecial {...props}/>;
+        case "FolderSpecialOutlined":
+            return <FolderSpecialOutlined {...props}/>;
     }
 }
+
+// <NavButton href='/dashboard/courses' icon='AutoStories'>Courses</NavButton>
