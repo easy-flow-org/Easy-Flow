6390183f135aeedb85ae2d8482ecf23a19504306
zippy0626 <zippy0626@gmail.com>
2025-12-07 22:41:04 -0500

    style: mobile style changes

--- app/dashboard/calendar/page.tsx
+++ app/dashboard/calendar/page.tsx
@@ -1,7 +1,9 @@
-export default function Dashboard() {
+import { Box, Typography } from "@mui/material";
+
+export default function Calendar() {
   return (
-    <>
-      
-    </>
+    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height:"calc(100vh - 56px)" }}>
+      <Typography variant="h4" textAlign={"center"}>Calendar feature is not implemented at this moment</Typography>
+    </Box>
   )
 }
